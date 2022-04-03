78750c9a89a452821afc858445c53014dcb152b8
geoffjecrois <je.suis.geoffrey.hach@gmail.com>
2022-04-03 19:16:50 +0200

    innit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function showPanel() {
+   const panel = document.querySelector('.panel');
+    if (panel.style.display=="block")
+            panel.style.display="none";
+    else
+    panel.style.display="block";
+        
+    
+    
+}
