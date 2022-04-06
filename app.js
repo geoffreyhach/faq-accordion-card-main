3a0a7d67e8e7f4aeac42df4a4cb40e28e8ea195a
geoffjecrois <je.suis.geoffrey.hach@gmail.com>
2022-04-06 11:17:10 +0200

    oui

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 
+const accordionTitles = document.querySelectorAll('.accordion-title');
 
-function showPanel() {
-   const panel = document.querySelector('.panel');
-    if (panel.style.display=="block")
-            panel.style.display="none";
-    else
-    panel.style.display="block";
+accordionTitles.forEach((n) => {
+    n.addEventListener('click', () => {
+        n.classList.toggle('active'); // add or remove the active class
+        // we need to calculate the height of the answer
+        const height = n.nextElementSibling.scrollHeight;
+
+        if(n.classList.contains('active')) // if the classe active exist, we change the max height
         
-    
-    
-}
+        n.nextElementSibling.style.maxHeight=`${height}px`;
+        else
+        n.nextElementSibling.style.maxHeight='0px';
+    });
+});
